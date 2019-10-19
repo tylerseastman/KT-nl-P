7ded1f7c34fc92abe7d20f5110225423166f3db8
shanjng <shanjng@umich.edu>
2019-10-19 12:58:56 -0400

    put data in folder and can now find top 10 most active posters

--- find-most-active.py
+++ find-most-active.py
@@ -1,25 +1,47 @@
 import json
+import numpy as np
+import os
 from collections import Counter 
 
 def main(): 
     freq = {}
 
-    data = json.loads(open('./random.json').read())
+    dirs = os.listdir("data")
+    print(dirs)
     users = json.loads(open('./users.json').read())
 
-    for message in data['messages']:
-        if (message['user'] in freq):
-            freq[message['user']] += 1
-        else: 
-            freq[message['user']] = 1
+    for fileName in dirs:
+        data = json.loads(open('./data/' + fileName).read())
+        print(fileName)
+        for message in data['messages']:
+            if('user' not in message):
+                pass
+            elif (message['user'] in freq):
+                freq[message['user']] += 1
+            else: 
+                freq[message['user']] = 1
+        print("finished")
 
     k = Counter(freq)
+    n = 10
     high = k.most_common(10) 
 
+    print("Top " + str(n) + " Posters in Random")
     for i in high:
         print(i[0]," :",i[1]," ") 
 
-    
+    ids = []
+    for i in high:
+        ids.append(i[0])
+
+
+    for i in range(len(ids)):
+        for user in users['members']:
+            if (ids[i] ==  user['id']):
+                ids[i] = user['real_name']
+
+    for x in ids:
+        print(x)
 
 main()
 
